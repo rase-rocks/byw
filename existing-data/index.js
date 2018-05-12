d0bccb109adac67e96d60d1997ed816e9872a1a4
Rob Atherton <rob@rase.rocks>
2018-05-12 09:00:58 +0100

    Added sample data and script to extract from csv file

---
+++ existing-data/index.js
@@ -0,0 +1,41 @@
+/*
+*
+* A Node script to convert from raw json (which is taken from csv file) into cut down version
+* ready for use in the API
+*
+* Note: The paths used are relative to root as this script is run by a npm run command from 
+* package.json
+*/
+
+const fs = require("fs");
+const csvFilePath = "./existing-data/Outlet Data 110518.csv";
+const csv = require("csvtojson");
+
+const locations = [];
+
+console.log("** Starting Conversion - ", csvFilePath);
+
+csv()
+    .fromFile(csvFilePath)
+    .on("json", (location )=> {
+        locations.push(location);
+    })
+    .on("done", (error) => {
+
+        const output = locations.map(outlet => {
+            return {
+                name: outlet.Name,
+                address: outlet.Address,
+                coordinates: [outlet.Latitude, outlet.Longitude],
+                category: outlet.Category
+            };
+        });
+        
+        fs.writeFileSync("./static-api/public/data", JSON.stringify(output));
+
+        console.log("** Completed with error: ", (error) ? error : "None");
+
+    });
+
+
+
