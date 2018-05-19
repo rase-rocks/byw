e853eba89fdf5951d5a47d90f96b6a5d698df650
Rob Atherton <rob@rase.rocks>
2018-05-19 14:57:03 +0100

    Updated comments

--- client/app/core/model/haversine-distance.js
+++ client/app/core/model/haversine-distance.js
@@ -40,6 +40,12 @@ function haversineDistance(coordinate1, coordinate2) {
 // Simplified sorting function does not allow for equal distance but does
 // cut out another haversineDistance call to determine greater or equal to
 // should be good enough for our purposes
+//
+// The coordinateFn is passed to allow non GeoJson objects with coordinate
+// arrays (like our data source) to use the same sorting function
+// The coordinateFn should take an object of the intended sort type and return
+// a an array of numbers encoding the longitude and latitude in that order 
+// as per GeoJson
 function makeArraySort(basePoint, coordinateFn) {
     return function compare(a, b) {
         
