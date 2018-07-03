fa3be7a04585c1ed6556198352f95089e70e2ff8
Rob Atherton <rob@rase.rocks>
2018-07-03 09:36:55 +0100

    Docs alteration

--- build-settings/build-settings.js
+++ build-settings/build-settings.js
@@ -1,5 +1,5 @@
 const cssFilename = "byw.style.min.css";
-const cssLinkString = `<link href="/css/${cssFilename}" type="text/css" rel="stylesheet">`;
+const cssLinkString = `<link href="css/${cssFilename}" type="text/css" rel="stylesheet">`;
 const keywords = "welsh,cymraeg,language,find,locate,speak,rate,review";
 const description = "Find and visit Welsh speaking places";
 const title = "Breath Your Welsh";
--- build-settings/build.sh
+++ build-settings/build.sh
@@ -17,12 +17,12 @@ node ./s3-build/static-index-build.js > ./s3-build/index.html
 echo "\n[3/5]-[${GREEN}Building client app${NC}]\n"
 gulp static
 
+echo "\n[4/5]-[${GREEN}Cleaning up${NC}]\n"
+rm ./s3-build/static-index-build.js
+
 echo "\n[2/5]-[${GREEN}Copying build to /docs for GitHub pages${NC}]\n"
 rm -r ./docs
 mkdir ./docs
 cp -r ./s3-build/* ./docs
-
-echo "\n[4/5]-[${GREEN}Cleaning up${NC}]\n"
-rm ./s3-build/static-index-build.js
 
 echo "\n[5/5]-[Build complete at ${GREEN}s3-build/${NC}]\n"
