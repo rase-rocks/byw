a0877794e61312b5bd3125ff65cf473576f674d7
Rob Atherton <rob@rase.rocks>
2018-07-03 12:06:28 +0100

    Added script to make alterations for gh-pages on each build

--- build-settings/build.sh
+++ build-settings/build.sh
@@ -7,22 +7,23 @@ GREEN='\033[1;32m'
 
 clear
 
-echo "\n[1/5]-[${RED}Removing Build folder${NC}]\n"
+echo "\n[1/6]-[${RED}Removing Build folder${NC}]\n"
 rm -r ./s3-build
 
-echo "\n[2/5]-[${GREEN}Rendering index.html${NC}]\n"
+echo "\n[2/6]-[${GREEN}Rendering index.html${NC}]\n"
 gulp static-build
 node ./s3-build/static-index-build.js > ./s3-build/index.html
 
-echo "\n[3/5]-[${GREEN}Building client app${NC}]\n"
+echo "\n[3/6]-[${GREEN}Building client app${NC}]\n"
 gulp static
 
-echo "\n[4/5]-[${GREEN}Cleaning up${NC}]\n"
+echo "\n[4/6]-[${GREEN}Cleaning up${NC}]\n"
 rm ./s3-build/static-index-build.js
 
-echo "\n[2/5]-[${GREEN}Copying build to /docs for GitHub pages${NC}]\n"
+echo "\n[5/6]-[${GREEN}Copying build to /docs for GitHub pages${NC}]\n"
 rm -r ./docs
 mkdir ./docs
 cp -r ./s3-build/* ./docs
+sed -i '' "s/assets/byw\/assets/g; s/static-api/byw\/static-api/g;" ./docs/bundle.js
 
-echo "\n[5/5]-[Build complete at ${GREEN}s3-build/${NC}]\n"
+echo "\n[6/6]-[Build complete at ${GREEN}s3-build/${NC}]\n"
